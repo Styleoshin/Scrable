84dcabfd5fa87354ab5bf7105b8c7ea9c2b66153
Yacine <yacine.benaffane@hotmail.com>
2017-11-01 15:21:06 +0100

    Implement class Tile

--- src/sample/Square.java
+++ src/sample/Square.java
@@ -1,7 +1,37 @@
 package sample;
 
+class Tile {
+    private char letter ;
+    private int score = 0;
+    private boolean isBlank = false;
+    private  boolean placed = false;
+
+    Tile(char letter, int score, boolean isBlank) {
+        this.letter = letter;
+        this.score = score;
+        this.isBlank = isBlank;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean isBlank() {
+        return isBlank;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+}
+
 public class Square extends Sprite {
     private boolean tileLocked = false;
+    private Tile tile = null;
 
     Square(String filenameImage) {
         super(filenameImage);
@@ -14,4 +44,9 @@ public class Square extends Sprite {
     public void setTileLocked(boolean tileLocked) {
         this.tileLocked = tileLocked;
     }
+        Square(String filenameImage, char letter, int score, boolean isBlank){
+            super(filenameImage);
+            tile = new Tile(letter, score, isBlank);
+        }
 }
+
