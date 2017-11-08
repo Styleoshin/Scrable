7597ef3a6eec2f4d52501c2e568e3441981acf15
Yacine <yacine.benaffane@hotmail.com>
2017-11-08 18:33:41 +0100

    Implement class NetworkClient

---
+++ src/NetworkClient.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class NetworkClient {
+
+    private Socket socket;
+    
+    public void connect(String ip, int port ) throws IOException {
+      socket = new Socket(ip,port);
+    }
+}
