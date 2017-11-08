b557ec021bfcf03ab4e516374ca903df7cd6c69a
Yacine <yacine.benaffane@hotmail.com>
2017-11-08 15:23:11 +0100

    Implement abstract class Player

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+abstract public class Player {
+    private int score = 0;
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+    private void addScore(int newScore ){
+        score+=newScore;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
