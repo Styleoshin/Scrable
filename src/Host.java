239f91e26439ae9c422a1b73ecd494fcd0534bb2
Yacine <yacine.benaffane@hotmail.com>
2017-11-08 18:28:46 +0100

    Implement class Host

---
+++ src/Host.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Host extends Player {
+
+    private NetworkServer network;
+
+    Host(String name, int port, int numberPlayer) throws IOException {
+        super(name);
+        network.connect(port, numberPlayer);
+    }
+}
+
