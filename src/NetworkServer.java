e3323db5e13c490a0ea8b0d6e3b1bdc5fa0bb9bf
Yacine <yacine.benaffane@hotmail.com>
2017-11-08 18:26:08 +0100

    Implement class NetworkServer

---
+++ src/NetworkServer.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class NetworkServer {
+
+    private ServerSocket server;
+    private Socket socket ;
+
+    NetworkServer() throws IOException {
+        socket = server.accept();
+    }
+
+    public void connect(int port, int numberPlayer) throws IOException {
+            server = new ServerSocket(port, numberPlayer);
+        }
+}
+
