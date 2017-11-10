91ce28d1b3eaa544eeac7d6d75504c75ce77e489
Yacine <yacine.benaffane@hotmail.com>
2017-11-10 21:20:27 +0100

    Add threads socket for server

--- src/sample/Network/NetworkServer.java
+++ src/sample/Network/NetworkServer.java
@@ -4,6 +4,28 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+class AcceptClient implements Runnable {
+
+    private ServerSocket socketserver;
+    private Socket socket;
+    public AcceptClient(ServerSocket s){
+        socketserver = s;
+    }
+
+    public void run() {
+
+        try {
+            while(true){
+                socket = socketserver.accept();
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class NetworkServer {
 
     private ServerSocket server;
@@ -15,7 +37,8 @@ public class NetworkServer {
         }
 =======
         server = new ServerSocket(port, numberPlayer);
-        socket = server.accept();
+        Thread t = new Thread(new AcceptClient(server));
+        t.start();
     }
 >>>>>>> d4c31f4... Fix crash Network
 }
